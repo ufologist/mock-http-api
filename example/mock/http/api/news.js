f1df76a107c6dd8ddb648db365887fb58922c9fe
ufologist <klsforever@gmail.com>
2019-01-03 16:21:15 +0800

    添加 function 类型的 mock 示例

--- example/mock/http/api/news.js
+++ example/mock/http/api/news.js
@@ -7,6 +7,17 @@ module.exports = {
                     "title": "@ctitle"
                 }
             }
+        },
+        "GET /api/new/:id": function(request, response, next) {
+            // 可以返回对象, 达到动态组装 mock 数据的目的
+            return {
+                id: request.params.id,
+                name: '@cname'
+            };
+        },
+        "GET /api/new/:id/view": function(request, response, next) {
+            // 不返回结果时, 可以完全自己控制要输出的内容
+            response.send('view ' + request.params.id);
         }
     }
 };
